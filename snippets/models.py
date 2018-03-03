e0ee0235f1c1d7013919952c611391bfb0b3052f
Athanasios Garyfalos <thanos.egw@gmail.com>
2018-03-03 22:46:38 +0100

    Working version with Jason and non Jason data requests

--- snippets/models.py
+++ snippets/models.py
@@ -1,47 +1,19 @@
-from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
-from pygments.lexers import PythonLexer
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
-    lastTransaction = models.DateTimeField(auto_now_add=True)
-    highlighted = models.TextField()
-    textField = models.TextField(blank=False,
-                                 help_text='Rows to modify.')
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-    counterBefore = models.PositiveSmallIntegerField(default=0)
-    counterAfter = models.PositiveSmallIntegerField(default=0)
-
-    def save(self, *args, **kwargs):
-        """
-        Use the `pygments` library to create a highlighted HTML
-        representation of the code snippet.
-        """
-        self.highlighted = highlight(self.textField, PythonLexer(), HtmlFormatter(),)
-        super(Snippet, self).save(*args, **kwargs)
-
-    class Meta:
-        ordering = ('lastTransaction',)
-
-'''
-from __future__ import unicode_literals
-
-from django.db import models
-
-
-class Snippet(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    highlighted = models.TextField()
+    title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-
-    def save(self, *args, **kwargs):
-        """
-        Use the `pygments` library to create a highlighted HTML
-        representation of the code snippet.
-        """
-        super(Snippet, self).save(*args, **kwargs)
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
-        ordering = ('created',)'''
+        ordering = ('created',)
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
-from snippets.models import Snippet
-from django.contrib.auth.models import User
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ('url', 'id', 'username', 'snippets')
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
-
     class Meta:
         model = Snippet
-        fields = ('id', 'textField', 'owner', 'highlight', 'highlighted')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
