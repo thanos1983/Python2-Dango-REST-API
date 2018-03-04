4a9a99f4d5e425022531a0f16617bfd0340370eb
Athanasios Garyfalos <thanos.egw@gmail.com>
2018-03-04 15:01:37 +0100

    Error from heroku regarding static files

--- thanosTest/settings.py
+++ thanosTest/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# All settings common to all environments
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Configure Django App for Heroku.
 # django_heroku.settings(locals())
